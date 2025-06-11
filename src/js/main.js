c4ab282bf67ab99ab004e8f6b62d5cf1295a24ac
orneto-incm <orneto11@gmail.com>
2025-06-12 01:02:52 +0530

    initial setup

---
+++ src/js/main.js
@@ -0,0 +1,14 @@
+import "./style.css";
+import javascriptLogo from "./javascript.svg";
+import viteLogo from "/vite.svg";
+import { setupCounter } from "./counter.js";
+
+document.querySelector("#app").innerHTML = `
+  <div>
+    <div class="card">
+      <button id="counter" type="button"></button>
+    </div>
+  </div>
+`;
+
+setupCounter(document.querySelector("#counter"));
